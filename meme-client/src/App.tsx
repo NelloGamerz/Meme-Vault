b3349b13c17f1406fb2f2a7d00b2ed22a26156c1
Karan Pareek <140229317+NelloGamerz@users.noreply.github.com>
2025-05-28 21:36:59 +0530

    Update App.tsx

--- meme-client/src/App.tsx
+++ meme-client/src/App.tsx
@@ -1,16 +1,12 @@
-import { Routes, Route, Navigate, useParams } from 'react-router-dom';
+import { Routes, Route, Navigate,useParams } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
 import { AuthPage } from './pages/AuthPage';
 import { MainPage } from './pages/MainPage';
 import { ProtectedRoute } from './components/auth/ProtectedRoute';
 import { ProfilePage } from './components/mainPage/ProfilePage';
-import MemeDetailPage from './pages/MemeDetailPage';
+import MemeDetailPage  from './pages/MemeDetailPage'; 
 import { ForgotPasswordForm } from './pages/ForgotPasswordForm';
 import PasswordResetPage from './pages/password-reset';
-import { Layout } from './components/layout/Layout';
-import { ExplorePage } from './pages/ExplorePage';
-import { CreatePage } from './pages/CreatePage';
-import { SettingsPage } from './pages/SettingsPage';
 
 const RedirectToProfile = () => {
   const { userId } = useParams<{ userId: string }>();
@@ -21,74 +17,26 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path="/forgot-password\" element={<ForgotPasswordForm />} />
+        <Route path="/forgot-password" element={<ForgotPasswordForm />} />
         <Route path="/reset-password" element={<PasswordResetPage />} />
         <Route path="/reset-password/:id" element={<PasswordResetPage />} />
         <Route path="/auth" element={<AuthPage />} />
-        <Route
-          path="/"
-          element={
-            <ProtectedRoute>
-              <Layout>
-                <MainPage />
-              </Layout>
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/explore"
-          element={
-            <ProtectedRoute>
-              <Layout>
-                <ExplorePage />
-              </Layout>
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/create"
-          element={
-            <ProtectedRoute>
-              <Layout>
-                <CreatePage />
-              </Layout>
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/settings"
-          element={
-            <ProtectedRoute>
-              <Layout>
-                <SettingsPage />
-              </Layout>
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/meme/:id"
-          element={
-            <ProtectedRoute>
-              <Layout>
-                <MemeDetailPage />
-              </Layout>
-            </ProtectedRoute>
-          }
-        />
-        <Route path="/api/profile/:userId" element={<RedirectToProfile />} />
-        <Route
-          path="/profile/:userId"
-          element={
-            <Layout>
-              <ProfilePage />
-            </Layout>
-          }
-        />
-        <Route path="*" element={<Navigate to="/\" replace />} />
+        <Route path="/" element={
+          <ProtectedRoute>
+            <MainPage />
+          </ProtectedRoute>
+        } />
+        <Route path="/meme/:id" element={
+          <ProtectedRoute>
+            <MemeDetailPage />
+          </ProtectedRoute>} />  // Added route for meme detail page
+          <Route path="/api/profile/:userId" element={<RedirectToProfile />} />
+          <Route path="/profile/:userId" element={<ProfilePage />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
       <Toaster position="top-right" />
     </>
   );
 }
 
-export default App;
+export default App
